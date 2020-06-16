9e6311ca4f103138d115ce03082f86d05219ae72
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-06-16 20:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if () {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if () {
     
   }
 }
